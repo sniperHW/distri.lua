cd674dbd157f94d94b59d6777d13b9fbb8177534
huangwei <huangwei@huangwei-Aspire-4745G.(none)>
2013-10-30 23:20:58 +0800

    none

--- client.lua
+++ client.lua
@@ -2,54 +2,39 @@ local registernet = assert(package.loadlib("./luanet.so","RegisterNet"))
 registernet()
 dofile("net/net.lua")
 
-local ava_delay = 0
-local last_print = 0
-local total_recv = 0;
-local connection_set = {}
+local recv_count = 0;
 
-function remove_connection(connection)
-	local idx = 0
-	for k,v in pairs(connection_set) do
-		if v == connection then
-			idx = k
-		end
+function connect_ok(connection)
+	if connection then
+		SendPacket(connection,"hello kenny")
+	else
+		print("connect timeout")
 	end
-	if idx >= 1 then
-		table.remove(connection_set,idx)
-	end
-end
-
-function add_connection(connection)
-	table.insert(connection_set,connection)
 end
 
 function process_packet(connection,packet)
-	total_recv = total_recv + 1
+	recv_count = recv_count + 1
+	SendPacket(connection,"hello kenny")
 end
 
-function clientsSend()
-	for k,v in pairs(connection_set) do
-		SendPacket(v,"hello")				
-	end
+function _timeout(connection)
+	active_close(connection)
 end
 
 function mainloop()
-	local n = net:new(process_packet,remove_connection)
+	local n = net:new(process_packet)
 	net._send_timeout = _timeout
 	net._recv_timeout = _timeout
 	for i=1,arg[3] do
 		n:connect(add_connection,arg[1],arg[2],500)
 	end
+	local lasttick = GetSysTick()
 	while n:run(50) == 0 do
-		local nowtick = GetSysTick()
-		if nowtick >= last_print+1000 then
-			print("ava_delay:" .. ava_delay)
-			print("total recv:" .. total_recv)
-			ava_delay = 0
-			total_recv = 0
-			last_print = nowtick
+		local tick = GetSysTick()
+		if tick - 1000 >= lasttick then
+			print("recv_count:" .. recv_count)
+			lasttick = tick
 		end
-		clientsSend()
 	end
 	n = nil
 	print("main loop end")
--- echo.lua
+++ echo.lua
@@ -1,49 +1,35 @@
 local registernet = assert(package.loadlib("./luanet.so","RegisterNet"))
 registernet()
 dofile("net/net.lua") 
-connection_set = {}
-function remove_connection(connection)
-	local idx = 0
-	for k,v in pairs(connection_set) do
-		if v == connection then
-			idx = k
-		end
-	end
-	if idx >= 1 then
-		table.remove(connection_set,idx)
-	end
-end
-
-function add_connection(connection)
-	table.insert(connection_set,connection)
-end
 
 function process_packet(connection,packet)
 	send2all(connection_set,packet)
 end
 
-local totalsend = 0
-function send2all(packet)
-	for k,v in pairs(connection_set) do
-		SendPacket(v,packet)
-		totalsend = totalsend + 1
-	end
-end
+client_count = 0;
+
 
 function _timeout(connection)
 	active_close(connection)
 end
 
+function client_come(connection)
+	client_count = client_count + 1
+end
+
+function client_go(connection)
+	client_count = client_count - 1
+end
+
 function mainloop()
 	local lasttick = GetSysTick()
-	local n = net:new(process_packet,remove_connection):listen(add_connection,arg[1],arg[2])
+	local n = net:new(process_packet,client_go):listen(client_come,arg[1],arg[2])
 	net._send_timeout = _timeout
 	net._recv_timeout = _timeout
 	while n:run(50) == 0 do
 		local tick = GetSysTick()
 		if tick - 1000 >= lasttick then
-			print("client:" .. #connection_set .. " packet send:" ..totalsend)
-			totalsend = 0;
+			print("client:" .. client_count)
 			lasttick = tick
 		end
 	end
