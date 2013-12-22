7d4e42d6896a3b2376a76f63ae63460ce7f7f7a2
huangwei <huangwei@huangwei-Aspire-4745G.(none)>
2013-12-22 22:06:47 +0800

    none

--- kendynet/core/src/msg_que.c
+++ kendynet/core/src/msg_que.c
@@ -12,7 +12,7 @@ enum{
 uint32_t msgque_flush_time = 50;
 
 //每个线程都有一个per_thread_que与que关联
-struct per_thread_que
+typedef struct per_thread_que
 {
     union{
         struct write_que{
@@ -27,26 +27,26 @@ struct per_thread_que
     struct link_list local_que;
 	msgque_t que;
 	uint8_t     mode;//MSGQ_READ or MSGQ_WRITE
-};
+}*ptq_t;
 
 //一个线程使用的所有msg_que所关联的per_thread_que
-struct per_thread_struct
+typedef struct per_thread_struct
 {
     struct double_link_node hnode;//用于链入heart_beat.thread_structs
 	struct double_link  per_thread_que;
 	pthread_t thread_id;
-};
+}*pts_t;
 
 static pthread_key_t g_msg_que_key;
 static pthread_once_t g_msg_que_key_once = PTHREAD_ONCE_INIT;
 
 #ifdef MQ_HEART_BEAT
 //心跳处理，用于定时向线程发送信号冲刷msgque
-struct heart_beat
+typedef struct heart_beat
 {
     struct double_link  thread_structs;
     mutex_t mtx;
-};
+}*hb_t;
 
 static struct heart_beat *g_heart_beat;
 static pthread_key_t g_heart_beat_key;
@@ -59,7 +59,7 @@ static void* heart_beat_routine(void *arg){
 	    if(dn){
             while(dn != &g_heart_beat->thread_structs.tail)
             {
-                struct per_thread_struct *pts =(struct per_thread_struct*)dn;
+                pts_t pts =(pts_t)dn;
                 pthread_kill(pts->thread_id,SIGUSR1);
                 dn = dn->next;
             }
@@ -93,7 +93,7 @@ static void heart_beat_once_routine(){
 	thread_run(heart_beat_routine,NULL);
 }
 
-static inline struct heart_beat* get_heart_beat()
+static inline hb_t get_heart_beat()
 {
 	pthread_once(&g_heart_beat_key_once,heart_beat_once_routine);
     return g_heart_beat;
@@ -118,9 +118,9 @@ void delete_msgque(void *ud)
 	printf("on_que_destroy\n");
 }
 
-static inline struct per_thread_struct* get_per_thread_struct()
+static inline pts_t get_per_thread_struct()
 {
-	struct per_thread_struct *pts = (struct per_thread_struct*)pthread_getspecific(g_msg_que_key);
+    pts_t pts = (pts_t)pthread_getspecific(g_msg_que_key);
 	if(!pts){
 		pts = calloc(1,sizeof(*pts));
 		double_link_clear(&pts->per_thread_que);
@@ -128,7 +128,7 @@ static inline struct per_thread_struct* get_per_thread_struct()
 		pts->thread_id = pthread_self();
 #ifdef MQ_HEART_BEAT
 		//关联到heart_beat中
-        struct heart_beat *hb = get_heart_beat();
+        hb_t hb = get_heart_beat();
         mutex_lock(hb->mtx);
         double_link_push(&hb->thread_structs,&pts->hnode);
         mutex_unlock(hb->mtx);
@@ -138,9 +138,9 @@ static inline struct per_thread_struct* get_per_thread_struct()
 }
 
 //获取本线程，与que相关的per_thread_que
-static inline struct per_thread_que* get_per_thread_que(struct msg_que *que,uint8_t mode)
+static inline ptq_t get_per_thread_que(struct msg_que *que,uint8_t mode)
 {
-	struct per_thread_que *ptq = (struct per_thread_que*)pthread_getspecific(que->t_key);
+    ptq_t ptq = (ptq_t)pthread_getspecific(que->t_key);
 	if(!ptq){
 		ptq = calloc(1,sizeof(*ptq));
 		ptq->que = que;
@@ -152,17 +152,17 @@ static inline struct per_thread_que* get_per_thread_que(struct msg_que *que,uint
 }
 
 void msgque_release(msgque_t que){
-	struct per_thread_que *ptq = (struct per_thread_que*)pthread_getspecific(que->t_key);
+    ptq_t ptq = (ptq_t)pthread_getspecific(que->t_key);
 	if(ptq){
         if(ptq->mode == MSGQ_WRITE){
             ptq->write_que.flag = 1;
             double_link_remove(&ptq->write_que.pnode);
-            struct per_thread_struct *pts =  get_per_thread_struct();
+            pts_t pts =  get_per_thread_struct();
             if(double_link_empty(&pts->per_thread_que)){
                 pthread_setspecific(g_msg_que_key,NULL);
 #ifdef MQ_HEART_BEAT
                 //从heart_beat中移除
-                struct heart_beat *hb = get_heart_beat();
+                hb_t hb = get_heart_beat();
                 mutex_lock(hb->mtx);
                 double_link_remove(&pts->hnode);
                 mutex_unlock(hb->mtx);
@@ -212,14 +212,14 @@ void close_msgque(struct msg_que *que)
     mutex_unlock(que->mtx);
     struct double_link_node *dbn = double_link_pop(&que->blocks);
     while(dbn){
-        struct per_thread_que *ptq = (struct per_thread_que*)dbn;
+        ptq_t ptq = (ptq_t)dbn;
         condition_signal(ptq->read_que.cond);
         dbn = double_link_pop(&que->blocks);
     }
 }
 
 //push消息并执行同步操作
-static inline int8_t msgque_sync_push(struct per_thread_que *ptq)
+static inline int8_t msgque_sync_push(ptq_t ptq)
 {
     assert(ptq->mode == MSGQ_WRITE);
     if(ptq->mode != MSGQ_WRITE) return 0;
@@ -235,7 +235,7 @@ static inline int8_t msgque_sync_push(struct per_thread_que *ptq)
 		struct double_link_node *l = double_link_pop(&que->blocks);
 		if(l){
 			//if there is a block per_thread_struct wake it up
-            struct per_thread_que *block_ptq = (struct per_thread_que*)l;
+            ptq_t block_ptq = (ptq_t)l;
 			mutex_unlock(que->mtx);
             condition_signal(block_ptq->read_que.cond);
 			return 0;
@@ -245,43 +245,43 @@ static inline int8_t msgque_sync_push(struct per_thread_que *ptq)
 	return 0;
 }
 
-static inline int8_t msgque_sync_pop(struct per_thread_que *ptq,uint32_t timeout)
+static inline int8_t msgque_sync_pop(ptq_t ptq,uint32_t timeout)
 {
     assert(ptq->mode == MSGQ_READ);
     if(ptq->mode != MSGQ_READ) return 0;
     struct msg_que *que = ptq->que;
         mutex_lock(que->mtx);
         if(link_list_is_empty(&que->share_que) && timeout){
-                uint32_t end = GetSystemMs() + timeout;
-		while(link_list_is_empty(&que->share_que) && !que->wait4destroy){
-                        double_link_push(&que->blocks,&ptq->read_que.bnode);
-                        if(0 != condition_timedwait(ptq->read_que.cond,que->mtx,timeout)){
-                                //timeout
-                double_link_remove(&ptq->read_que.bnode);
-                                break;
-                        }
-			uint32_t l_now = GetSystemMs();
-			if(l_now < end) timeout = end - l_now;
-			else break;//timeout
+            uint32_t end = GetSystemMs() + timeout;
+            while(link_list_is_empty(&que->share_que) && !que->wait4destroy){
+                double_link_push(&que->blocks,&ptq->read_que.bnode);
+                if(0 != condition_timedwait(ptq->read_que.cond,que->mtx,timeout)){
+                    //timeout
+                    double_link_remove(&ptq->read_que.bnode);
+                    break;
                 }
+                uint32_t l_now = GetSystemMs();
+                if(l_now < end) timeout = end - l_now;
+                else break;//timeout
+            }
         }
         if(!que->wait4destroy && !link_list_is_empty(&que->share_que))
-        link_list_swap(&ptq->local_que,&que->share_que);
+            link_list_swap(&ptq->local_que,&que->share_que);
         mutex_unlock(que->mtx);
         if(que->wait4destroy) return -1;
         return 0;
 }
 
-static inline void before_put(struct per_thread_que *ptq)
+static inline void before_put(ptq_t ptq)
 {
-	struct per_thread_struct *pts = get_per_thread_struct();
+    pts_t pts = get_per_thread_struct();
     double_link_push(&pts->per_thread_que,&ptq->write_que.pnode);
 }
 
 int8_t msgque_put(msgque_t que,list_node *msg)
 {
 	if(que->wait4destroy)return -1;
-	struct per_thread_que *ptq = get_per_thread_que(que,MSGQ_WRITE);
+    ptq_t ptq = get_per_thread_que(que,MSGQ_WRITE);
 	assert(ptq->mode == MSGQ_WRITE);
     if(ptq->mode != MSGQ_WRITE)return 0;
     ptq->write_que.flag = 1;
@@ -297,7 +297,7 @@ int8_t msgque_put(msgque_t que,list_node *msg)
 int8_t msgque_put_immeda(msgque_t que,list_node *msg)
 {
 	if(que->wait4destroy)return -1;
-	struct per_thread_que *ptq = get_per_thread_que(que,MSGQ_WRITE);
+    ptq_t ptq = get_per_thread_que(que,MSGQ_WRITE);
 	assert(ptq->mode == MSGQ_WRITE);
     if(ptq->mode != MSGQ_WRITE)return 0;
     ptq->write_que.flag = 1;
@@ -311,7 +311,7 @@ int8_t msgque_put_immeda(msgque_t que,list_node *msg)
 int8_t msgque_get(msgque_t que,list_node **msg,uint32_t timeout)
 {
 	if(que->wait4destroy)return -1;
-	struct per_thread_que *ptq = get_per_thread_que(que,MSGQ_READ);
+    ptq_t ptq = get_per_thread_que(que,MSGQ_READ);
 	assert(ptq->mode == MSGQ_READ);
     if(ptq->mode != MSGQ_READ)return 0;
 	//本地无消息,执行同步
@@ -324,7 +324,7 @@ int8_t msgque_get(msgque_t que,list_node **msg,uint32_t timeout)
 	return 0;
 }
 
-static inline void _flush_local(struct per_thread_que *ptq)
+static inline void _flush_local(ptq_t ptq)
 {
     assert(ptq->mode == MSGQ_WRITE);
     if(ptq->que->wait4destroy)return;
@@ -335,10 +335,10 @@ static inline void _flush_local(struct per_thread_que *ptq)
 
 void msgque_flush()
 {
-    struct per_thread_struct *pts = get_per_thread_struct();
+    pts_t pts = get_per_thread_struct();
     struct double_link_node *dln = double_link_first(&pts->per_thread_que);
     while(dln && dln != &pts->per_thread_que.tail){
-        struct per_thread_que *ptq = (struct per_thread_que*)dln;
+        ptq_t ptq = (ptq_t)dln;
         ptq->write_que.flag = 1;_flush_local(ptq);ptq->write_que.flag = 0;
         dln = dln->next;
     }
@@ -349,10 +349,10 @@ void msgque_flush()
 void heart_beat_signal_handler(int sig)
 {
     block_sigusr1();
-	struct per_thread_struct *pts = get_per_thread_struct();
+    pts_t pts = get_per_thread_struct();
 	struct double_link_node *dln = double_link_first(&pts->per_thread_que);
 	while(dln && dln != &pts->per_thread_que.tail){
-		struct per_thread_que *ptq = (struct per_thread_que*)dln;
+        ptq_t ptq = (ptq_t)dln;
         if(ptq->write_que.flag == 0){
             _flush_local(ptq);
 		}else{
