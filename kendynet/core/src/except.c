4796049a4d1052cb88eeddaeb254cb619a50ae60
huangwei <huangweilook@21cn.com>
2014-01-01 01:58:20 +0800

    none

--- kendynet/core/src/except.c
+++ kendynet/core/src/except.c
@@ -103,6 +103,7 @@ void exception_throw(int32_t code,const char *file,const char *func,int32_t line
             if(strstr(strings[i],"main+"))
                 break;
         }
+		for(i = 0; i < sz; ++i) free(strings[i]);
         longjmp(frame->jumpbuffer,code);
     }
 	else
