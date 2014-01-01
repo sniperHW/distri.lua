60ec3bd6e4b898fece0dc629a48a5a1de7aca526
huangwei <huangweilook21cn.com>
2014-01-01 16:59:49 +0800

    none

--- kendynet/core/src/tls.c
+++ kendynet/core/src/tls.c
@@ -13,15 +13,11 @@ static pthread_once_t g_tls_key_once = PTHREAD_ONCE_INIT;
 
 static void tls_destroy_fn(void *ud)
 {
-    struct tls_st *all_tls = (struct tls_st*)ud;
+    struct tls_st *thd_tls = (struct tls_st*)ud;
     uint16_t i = 0;
     for(; i < MAX_TLS_SIZE;++i)
-    {
-        if(all_tls[i] && all_tls[i].destroy_fn)
-        {
-            all_tls[i].destroy_fn(all_tls[i].tls_val);
-        }
-    }
+        if(thd_tls[i] && thd_tls[i].tls_val && thd_tls[i].destroy_fn)
+            thd_tls[i].destroy_fn(thd_tls[i].tls_val);
     free(ud);
 }
 
@@ -34,13 +30,14 @@ int32_t tls_create(uint16_t key,void *ud,TLS_DESTROY_FN fn)
 {
     if(key >= MAX_TLS_SIZE || !ud) return -1;
     pthread_once(&g_tls_key_once,tls_once_routine);
-    struct tls_st *all_tls = (struct tls_st *)pthread_getspecific(g_tls_key);
-    if(!all_tls){
-        all_tls = calloc(1,sizeof(struct tls_st*));
-        pthread_setspecific(g_tls_key,(void*)all_tls);
-    }
-    all_tls[key].tls_val = ud;
-    all_tls[key].destroy_fn = fn;
+    struct tls_st *thd_tls = (struct tls_st *)pthread_getspecific(g_tls_key);
+    if(!thd_tls){
+        thd_tls = calloc(1,sizeof(struct tls_st*));
+        pthread_setspecific(g_tls_key,(void*)thd_tls);
+    }else if(thd_tls[key].tls_val || thd_tls[key].destroy_fn)
+		return -1;
+    thd_tls[key].tls_val = ud;
+    thd_tls[key].destroy_fn = fn;
     return 0;
 }
 
@@ -49,23 +46,20 @@ void* tls_get(uint32_t key)
 {
     if(key >= MAX_TLS_SIZE) return NULL;
     pthread_once(&g_tls_key_once,tls_once_routine);
-    struct tls_st *all_tls = (struct tls_st *)pthread_getspecific(g_tls_key);
-    if(!all_tls){
-        all_tls = calloc(1,sizeof(struct tls_st*));
-        pthread_setspecific(g_tls_key,(void*)all_tls);
-    }
-    return all_tls[key].tls_val;
+    struct tls_st *thd_tls = (struct tls_st *)pthread_getspecific(g_tls_key);
+    if(!thd_tls) return NULL;
+    return thd_tls[key].tls_val;
 }
 
 int32_t  tls_set(uint32_t key,void *ud);
 {
     if(key >= MAX_TLS_SIZE) return -1;
     pthread_once(&g_tls_key_once,tls_once_routine);
-    struct tls_st *all_tls = (struct tls_st *)pthread_getspecific(g_tls_key);
-    if(!all_tls) return -1;
-	if(all_tls[key] && all_tls[key].destroy_fn)
-		all_tls[key].destroy_fn(all_tls[key].tls_val);
-	all_tls[key].tls_val = ud;
+    struct tls_st *thd_tls = (struct tls_st *)pthread_getspecific(g_tls_key);
+    if(!thd_tls) return -1;
+	if(thd_tls[key] && thd_tls[key].destroy_fn)
+		thd_tls[key].destroy_fn(thd_tls[key].tls_val);
+	thd_tls[key].tls_val = ud;
 	return 0;	
 }
 
