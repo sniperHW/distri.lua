1d792557a69a3a35709e8c7eb54fdcd05aa1e97a
huangwei <huangwei@huangwei-Aspire-4745G.(none)>
2014-02-08 22:48:40 +0800

    none

--- big challenge/gameserver/avatar.h
+++ big challenge/gameserver/avatar.h
@@ -1,30 +1,59 @@
 #ifndef _AVATAR_H
 #define _AVATAR_H
 
-#include "game/aoi.h"
 #include "core/msgdisp.h"
 #include "common/agentsession.h"
 #include "core/refbase.h"
 #include "core/cstring.h"
 #include "core/rpacket.h"
 #include "core/wpacket.h"
-#include "core/dlist.h"
 
 #define MAX_PLAYER    8191*8  //superservice最多容纳8191*8个玩家对象
-#define MAX_NONPLAYER 1<<18   //battleservice最多容纳1<<18个非玩家对象，非玩家对象包括npc,掉落物等
 
 typedef struct avatarid{
+	uint16_t super_player_index;
 	union{
 		struct{
-			uint64_t index:18;
-			uint64_t avattype:8;        
-			uint64_t checksum:38;     
+			uint32_t isplayer:1;
+			uint32_t battleservice_id:6;        
+			uint32_t map_id:14;
+			uint32_t ojb_index:11;     
 		};
-		uint64_t     data;
+		uint32_t     data;
 	};
 }avatarid;
 
-enum{
+
+typedef struct player{
+	agentsession _agentsession;
+	string_t     _actname;
+	msgdisp_t    _msgdisp;
+	uint16_t     _index;
+}player,*player_t;
+
+static inline avatarid rpk_read_avatarid(rpacket_t rpk)
+{
+	avatarid _avatarid;
+	_avatarid.super_player_index = rpk_read_uint16(rpk);
+	_avatarid.data = rpk_read_uint32(rpk);
+	return _avatarid;
+}
+
+static inline avatarid rpk_reverse_read_avatarid(rpacket_t rpk)
+{
+	avatarid _avatarid;
+	reverse_read(rpk,&_avatarid,3);
+	return _avatarid;	
+}
+
+static inline void wpk_write_avatarid(wpacket_t wpk,avatarid _avatarid)
+{
+	wpk_write_uint16(wpk,_avatarid.super_player_index);
+	wpk_write_uint32(wpk,_avatarid.data);
+}
+
+
+/*enum{
 	avatar_player = 1,
 	avatar_npc,
 };
@@ -75,5 +104,5 @@ player_t cast2player(avatarid);
 void     release_player(player_t _player);
 
 typedef void (*player_cmd_handler)(player_t,rpacket_t);
-
+*/
 #endif
