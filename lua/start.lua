d902b23aff69aa820f52795c6ea388309ecace85
sniperHW <huangweilook@21cn.com>
2014-04-23 15:05:58 +0800

    Update start.lua
    none

--- lua/start.lua
+++ lua/start.lua
@@ -3,6 +3,9 @@ local Sche = require "lua/scheduler"
 --启动一个light process运行主文件
 Sche.Spawn(dofile,C.startfile)
 --运行消息循环
-while C.run(5) do
-	Sche.Schedule()
+local ms = 5
+while C.run(ms) do
+	if Sche.Schedule() > 0 then
+		ms = 0
+	end
 end
