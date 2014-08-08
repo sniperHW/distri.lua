ea7abe03c84355fb14cb6227abbaab93f4116780
sniperHW <huangweilook@21cn.com>
2014-08-08 15:45:19 +0800

    none

--- echoserver.lua
+++ echoserver.lua
@@ -1,8 +1,5 @@
 local Socket = require "lua/socket"
 
-
-local socket = Socket.new(luasocket.AF_INET,luasocket.SOCK_STREAM,luasocket.IPPROTO_TCP)
-
 local function on_packet(client,packet)
 	client:send(packet)
 end
@@ -11,14 +8,13 @@ local function on_disconnected(client,err)
 	print("client disconnected")
 end
 
-local function on_new_client(client)
+if not Socket.new(luasocket.AF_INET,
+				  luasocket.SOCK_STREAM,
+				  luasocket.IPPROTO_TCP):listen("127.0.0.1",8010,function (client)
 	client:set_packet_callback(on_packet)
 	client:set_disconn_callback(on_disconnected)
 	client:establish(4096)
-end
-
-local err = socket:listen("127.0.0.1",8010,on_new_client) 
-if not err then
+end) then
 	print("server listen on 127.0.0.1 8010")
 end
 
