d43b77bc3932b211be173db31ab1e0996bd00e78
sniperHW <huangweilook@21cn.com>
2014-08-22 15:29:44 +0800

    delete old

--- old/KendyNet/README.md
+++
@@ -1,1 +0,0 @@
-重构中，最新代码[请见](https://github.com/sniperHW/distri.lua/tree/master/refactoring/KendyNet)
--- old/thread2.lua
+++
@@ -1,8 +0,0 @@
-local Thread = require "lua/thread"
-local Sche = require "lua/scheduler"
-print("thread2 begin")
-Thread.RegThdFunction("Plus",
-function (arg)
-	return nil,arg[1] + arg[2]
-end
-)
