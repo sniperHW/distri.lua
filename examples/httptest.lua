0e364f1e28876db8a807fea571cdc37b9b68e098
sniperHW <huangweilook@21cn.com>
2014-08-26 23:46:10 +0800

    Update httptest.lua

--- examples/httptest.lua
+++ examples/httptest.lua
@@ -1,6 +1,6 @@
 local Socket = require "lua/socket"
 local Sche = require "lua/sche"
-
+local response = "HTTP/1.1 200 OK\r\nConnection: Keep-Alive\r\nContent-Type: text/plain\r\nDate: Tue, 26 Aug 2014 11:40:38 GMT\r\nTransfer-Encoding: chunked\r\n\r\nc\r\nHello World\n\r\n0\r\n\r\n"
 local count = 0
 local server = Socket.New(CSocket.AF_INET,CSocket.SOCK_STREAM,CSocket.IPPROTO_TCP)
 if not server:Listen("127.0.0.1",8001) then
@@ -15,7 +15,7 @@ if not server:Listen("127.0.0.1",8001) then
 						client:Close()
 						return					
 					elseif packet:ToString() == "ON_MESSAGE_COMPLETE" then
-						local wpk = CPacket.NewRawPacket("HTTP/1.1 200 OK\r\nConnection: Keep-Alive\r\nContent-Type: text/plain\r\nDate: Tue, 26 Aug 2014 11:40:38 GMT\r\nTransfer-Encoding: chunked\r\n\r\nc\r\nHello World\n\r\n0\r\n\r\n")
+						local wpk = CPacket.NewRawPacket(response)
 						client:Send(wpk)
 					end
 				end
