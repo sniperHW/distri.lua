9ca3a7b6bbf812a03daf57c277e9a67410a4bec3
sniperHW <huangweilook@21cn.com>
2015-04-09 16:06:39 +0800

    准备对数据报套接口的支持

--- lua/socket.lua
+++ lua/socket.lua
@@ -7,40 +7,10 @@ local Sche = require "lua.sche"
 local LinkQue  = require "lua.linkque"
 local socket = {}
 
-function socket:new(domain)
-  local o = {}
-  self.__index = self      
-  setmetatable(o,self)
-  o.luasocket = CSocket.new1(o,domain)
-  if not o.luasocket then
-	return nil
-  end
-  o.errno = 0
-  return o   
-end
+--function for stream socket
+local stream = {}
 
-function socket:new2(sock)
-  local o = {}
-  self.__index = self          
-  setmetatable(o, self)
-  o.luasocket = CSocket.new2(o,sock)
-  o.errno = 0 
-  return o
-end
-
---[[
-关闭socket对象，同时关闭底层的luasocket对象,这将导致连接断开。
-务必保证对产生的每个socket对象调用Close。
-]]--
-function socket:Close()
-	local luasocket = self.luasocket
-	if luasocket then
-		self.luasocket = nil
-		CSocket.close(luasocket)
-	end	
-end
-
-local function on_new_conn(self,sock)
+function stream.on_new_conn(self,sock)
 	self.new_conn:Push({sock})	
 	local co = self.block_onaccept:Pop()
 	if co then
@@ -48,10 +18,7 @@ local function on_new_conn(self,sock)
 	end
 end
 
---[[
-在ip:port上建立TCP监听
-]]--
-function socket:Listen(ip,port)
+function stream.Listen(self,ip,port)
 	if not self.luasocket then
 		return "socket close"
 	end
@@ -60,11 +27,113 @@ function socket:Listen(ip,port)
 	end
 	self.block_onaccept = LinkQue.New()
 	self.new_conn = LinkQue.New()
-	self.__on_new_connection = on_new_conn
+	self.__on_new_connection = stream.on_new_conn
+	self.Connect = nil
+	self.Accept = stream.Accept
 	return CSocket.listen(self.luasocket,ip,port)
 end
 
-local function process_c_disconnect_event(self,errno)
+function stream.cb_connect (self,s,err)
+	if not s or err ~= 0 then
+		self.errno = err
+	else
+		self.luasocket = CSocket.new2(self,s)
+		self.Establish = stream.Establish
+	end
+	local co = self.connect_co
+	if co then
+		self.connect_co = nil
+		Sche.WakeUp(co)--Schedule(co)
+	end
+end
+
+function stream.Connect(self,ip,port)
+	self.Listen = nil
+	self.Accept = nil
+	local err,ret = CSocket.connect(self.luasocket,ip,port)
+	if err then
+		return err
+	else
+		if ret == 0 then
+			self.connect_co = Sche.Running()
+			self.___cb_connect = stream.cb_connect
+			Sche.Block()
+		elseif ret == 1 then
+			--success immediately
+		end
+		if not self.luasocket or  self.errno ~= 0 then
+			return self.errno
+		else
+			return nil
+		end				
+	end
+end
+
+ function stream.Accept(self)
+	if not self.luasocket then
+		return nil,"socket close"
+	end
+	if not self.block_onaccept or not self.new_conn then
+		return nil,"invaild socket"
+	else	
+		while true do
+			local s = self.new_conn:Pop()
+			if s then
+			    s = s[1]
+				local sock = socket:new2(s)
+				sock.Establish = stream.Establish
+				return sock,nil
+			else
+				local co = Sche.Running()
+				self.block_onaccept:Push(co)
+				Sche.Block()
+				if  not self.luasocket then
+					return nil,"socket close" --socket被关闭
+				end				
+			end
+		end
+	end
+end
+
+function stream.Recv(self,timeout)
+	--[[
+	尝试从套接口中接收数据,如果成功返回数据,如果失败返回nil,错误描述
+	timeout参数如果为nil,则当socket没有数据可被接收时Recv调用将一直阻塞
+	直到有数据可返回或出现错误.否则在有数据可返回或出现错误之前Recv最少阻塞
+	timeout毫秒
+	]]--		
+	while true do--not self.closing do	
+		local packet = self.packet:Pop()
+		if packet then
+			return packet[1],nil
+		elseif not self.luasocket then
+			return nil,self.errno
+		end		
+		local co = Sche.Running()
+		co = {co}	
+		self.block_recv:Push(co)		
+		self.timeout = timeout
+		Sche.Block(timeout)
+		self.block_recv:Remove(co) --remove from block_recv
+		if self.timeout then
+			self.timeout = nil
+			return nil,"recv timeout"
+		end
+	end	
+end
+
+function stream.Send(self,packet)
+	--[[
+	将packet发送给对端，如果成功返回nil,否则返回错误描述.
+	(此函数不会阻塞,立即返回)
+	]]--	
+	if not self.luasocket then
+		return "socket socket"
+	end
+	return CSocket.send(self.luasocket,packet)
+end
+
+function stream.process_c_disconnect_event(self,errno)
 	self.errno = errno
 	local co
 	while self.block_noaccept do
@@ -97,10 +166,6 @@ local function process_c_disconnect_event(self,errno)
 			Sche.Schedule(v)
 		end					
 	end
-	--[[if self.application then
-		self.application.sockets[self] = nil
-		self.application = nil
-	end]]--
 	if self.on_disconnected then
 		if Sche.Running() then
 			self.on_disconnected(self,errno)
@@ -120,7 +185,7 @@ local function process_c_disconnect_event(self,errno)
 	end			
 end
 
-local function process_c_packet_event(self,packet)
+function stream.process_c_packet_event(self,packet)
 	self.packet:Push({packet})
 	local co = self.block_recv:Pop()
 	if co then
@@ -130,10 +195,9 @@ local function process_c_packet_event(self,packet)
 	end
 end
 
-function socket:Establish(decoder,recvbuf_size)
-	self.isestablish = true
-	self.__on_packet = process_c_packet_event
-	self.__on_disconnected = process_c_disconnect_event
+function stream.Establish(self,decoder,recvbuf_size)
+	self.__on_packet = stream.process_c_packet_event
+	self.__on_disconnected = stream.process_c_disconnect_event
 	self.block_recv = LinkQue.New()	
 	if not decoder then
 		decoder = CSocket.rawdecoder()
@@ -142,116 +206,51 @@ function socket:Establish(decoder,recvbuf_size)
 		recvbuf_size = 65535
 	end	
 	CSocket.establish(self.luasocket,recvbuf_size,decoder)
+	self.Send = stream.Send
+	self.Recv = stream.Recv
+	self.Establish = nil
 	self.packet = LinkQue.New()
 	return self	
+end				
+
+--function for datagram socket
+local datagram = {}
+
+function socket:new(domain,type,protocal)
+	local o = {}
+	self.__index = self      
+	setmetatable(o,self)
+	o.luasocket = CSocket.new1(o,domain,type,protocal)
+	if not o.luasocket then
+	return nil
+	end
+	o.errno = 0
+	if type == CSocket.SOCK_STREAM then
+		o.Listen = stream.Listen
+		o.Connect = stream.Connect
+	end
+	return o   
+end
+
+function socket:new2(sock)
+	local o = {}
+	self.__index = self          
+	setmetatable(o, self)
+	o.luasocket = CSocket.new2(o,sock)
+	o.errno = 0 
+	return o
 end
 
 --[[
-接受一个TCP连接,并将新连接的接收缓冲设为max_packet_size
+关闭socket对象，同时关闭底层的luasocket对象,这将导致连接断开。
+务必保证对产生的每个socket对象调用Close。
 ]]--
-function socket:Accept()
-	if not self.luasocket then
-		return nil,"socket close"
-	end
-
-	if not self.block_onaccept or not self.new_conn then
-		return nil,"invaild socket"
-	else	
-		while true do
-			local s = self.new_conn:Pop()
-			if s then
-			    s = s[1]
-				local sock = socket:new2(s)
-				--establish(sock,max_packet_size or 65535,decoder)
-				return sock,nil
-			else
-				local co = Sche.Running()
-				self.block_onaccept:Push(co)
-				Sche.Block()
-				if  not self.luasocket then
-					return nil,"socket close" --socket被关闭
-				end				
-			end
-		end
-	end
-end
-
-local function cb_connect(self,s,err)
-	if not s or err ~= 0 then
-		self.errno = err
-	else
-		self.luasocket = CSocket.new2(self,s)
-	end
-	local co = self.connect_co
-	if co then
-		self.connect_co = nil
-		Sche.WakeUp(co)--Schedule(co)
+function socket:Close()
+	local luasocket = self.luasocket
+	if luasocket then
+		self.luasocket = nil
+		CSocket.close(luasocket)
 	end	
-end
-
---[[
-尝试与ip:port建立TCP连接，如果连接成功建立，将连接的接收缓冲设为max_packet_size
-]]--
-function socket:Connect(ip,port)
-	local err,ret = CSocket.connect(self.luasocket,ip,port)
-	if err then
-		return err
-	else
-		if ret == 0 then
-			self.connect_co = Sche.Running()
-			self.___cb_connect = cb_connect
-			Sche.Block()
-		elseif ret == 1 then
-			--success immediately
-		end
-		if not self.luasocket or  self.errno ~= 0 then
-			return self.errno
-		else
-			return nil
-		end				
-	end
-end
-
---[[
-尝试从套接口中接收数据,如果成功返回数据,如果失败返回nil,错误描述
-timeout参数如果为nil,则当socket没有数据可被接收时Recv调用将一直阻塞
-直到有数据可返回或出现错误.否则在有数据可返回或出现错误之前Recv最少阻塞
-timeout毫秒
-]]--
-function socket:Recv(timeout)
-	if not self.isestablish then
-		return nil,"invaild socket"
-	else
-		while true do--not self.closing do	
-			local packet = self.packet:Pop()
-			if packet then
-				return packet[1],nil
-			elseif not self.luasocket then
-				return nil,self.errno
-			end		
-			local co = Sche.Running()
-			co = {co}	
-			self.block_recv:Push(co)		
-			self.timeout = timeout
-			Sche.Block(timeout)
-			self.block_recv:Remove(co) --remove from block_recv
-			if self.timeout then
-				self.timeout = nil
-				return nil,"recv timeout"
-			end
-		end	
-	end
-end
-
---[[
-将packet发送给对端，如果成功返回nil,否则返回错误描述.
-(此函数不会阻塞,立即返回)
-]]--
-function socket:Send(packet)
-	if not self.luasocket then
-		return "socket socket"
-	end
-	return CSocket.send(self.luasocket,packet)
 end
 
 function socket:tostring()
@@ -259,6 +258,6 @@ function socket:tostring()
 end
 
 return {
-	New = function (domain) return socket:new(domain) end
+	New = function (domain) return socket:new(domain,CSocket.SOCK_STREAM) end,
 }
 
