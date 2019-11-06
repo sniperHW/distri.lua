b7d5fe642c06eb51228b670ad05db77083a5adfb
sniperHW <sniperHW@163.com>
2019-11-06 17:03:05 +0800

    Update kn_refobj.c

--- KendyNet/src/kn_refobj.c
+++ KendyNet/src/kn_refobj.c
@@ -15,22 +15,9 @@ void refobj_init(refobj *r,void (*destructor)(void*))
 uint32_t refobj_dec(refobj *r)
 {
     volatile uint32_t count;
-    uint32_t c;
-    struct timespec ts;
     assert(r->refcount > 0);
     if((count = ATOMIC_DECREASE(&r->refcount)) == 0){
-        for(c = 0,r->identity = 0;;){
-            if(COMPARE_AND_SWAP(&r->flag,0,1))
-                break;
-            if(++c < 4000){
-                __asm__("pause");
-            }else{
-                c = 0;
-                ts.tv_sec = 0;
-                ts.tv_nsec = 500000;
-                nanosleep(&ts, NULL);
-            }
-        }
+        r->identity = 0;//立即清0,使得cast2refobj可以快速查觉
         r->destructor(r);
     }
     return count;
@@ -41,30 +28,23 @@ refobj *cast2refobj(ident _ident)
     refobj *ptr = NULL;
     if(unlikely(!_ident.ptr)) return NULL;
     TRY{
-              refobj *o = (refobj*)_ident.ptr;
-              uint32_t c = 0;
-    	struct timespec ts;
-              while(_ident.identity == o->identity){
-                    if(COMPARE_AND_SWAP(&o->flag,0,1)){
-                        if(_ident.identity == o->identity){
-                                if(__sync_fetch_and_add(&o->refcount,1) > 0)
-                                    ptr = o;
-                                else
-                                    o->refcount = 0;
-                        }
-                        o->flag = 0;
-                        break;
-                    }else{                     
-                     if(++c < 4000){
-                         __asm__("pause");
-                     }else{
-                        c = 0;
-                        ts.tv_sec = 0;
-                        ts.tv_nsec = 500000;
-                        nanosleep(&ts, NULL);
-                    }
-                }                
-              }
+        refobj *o = (refobj*)_ident.ptr;
+        /*
+         *   假设refcount == 1
+         *   A线程执行到COMPARE_AND_SWAP的前一条指令然后暂停,     
+         *   B执行ATOMIC_DECREASE,refcount == 0,此时无论r->identity = 0是否执行。当A恢复执行后最终必将会发现oldCount == 0跳出for循环
+         *
+         */
+        for(;_ident.identity == o->identity;) {
+            uint32_t oldCount = o->refcount;
+            uint32_t newCount = oldCount + 1;
+            if(oldCount == 0) {
+                break;
+            }
+            if(COMPARE_AND_SWAP(&o->refcount,oldCount,newCount)){
+                ptr = o;
+            }    
+        }
     }CATCH_ALL{
             ptr = NULL;      
     }ENDTRY;
